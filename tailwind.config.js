e2b7962b731e32426d6514e1ce0c5e8c40041680
Willz <Obiadichinwuba@yahoo.com>
2025-04-01 16:48:27 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//     content: [
+//         "./src/index.html",
+//         "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     ]
+//     theme: {
+//         extend: {
+//             maxWidth: {
+//                 '16': '16rem',
+//             }
+//         },
+//     },
+//     plugins: [
+//         require('@tailwindcss/forms'), 49.2k (gzipped: :) // ...
+
+
+//     ],
+// }
